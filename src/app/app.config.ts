4662f375a932472a71db6a127f7e627e6d07702d
Quest_man <mitrofanov551@gmail.com>
2025-07-10 17:25:47 +0300

    feature: localization service added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-
+export function createTranslateLoader(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      }),
+    ),
   ]
 };
--- src/app/features/greet-page/components/header-component/header-component.ts
+++ src/app/features/greet-page/components/header-component/header-component.ts
@@ -1,20 +1,45 @@
 import { Component } from '@angular/core';
-
+import { OnDestroy, OnInit } from '@angular/core';
+import { LocalizationService } from '../../../../core/localization-service';
+import { TranslateModule} from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header-component.html',
-  styleUrl: './header-component.scss'
+  styleUrl: './header-component.scss',
+  standalone: true
 })
-export class HeaderComponent {
-  isMenuOpen: boolean = false;
-  langMenuOpen: boolean = false;
+export class HeaderComponent implements OnInit, OnDestroy{
+  current_lang : string = 'en';
+  langNames!: string[];
+  languages: { [key: string]: string } = {};
+  
+  private sub = new Subscription;
+  constructor(private config: LocalizationService){}
+  ngOnInit(): void {
+    const LangSub = this.config.refresh_lang$.subscribe(lang => {
+      this.current_lang = lang;
+    })
+    this.sub.add(LangSub);
+    this.languages = this.config.supported_langs;
+    this.langNames = Object.keys(this.languages);
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  public isMenuOpen: boolean = false;
+  public langMenuOpen: boolean = false;
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
   toggleLangMenu() {
     this.langMenuOpen = !this.langMenuOpen;
   }
+  setLanguage(lang: string){
+    this.config.setLanguage(lang);
+  }
+  
   nav_items: string[] = ['Something', 'Contacts'];
+  
 }
-
