f3d6ea990dd25ef152a20a2c0fac056cdfd2102b
Quest_man <mitrofanov551@gmail.com>
2025-08-13 20:51:24 +0300

    feature: menu layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router'
 import { GreetPage } from './features/greet-page/greet-page'
 import { MessangerPage } from './features/messanger-page/messanger-page'
 export const routes: Routes = [
-    { path: '', component: GreetPage },
-    { path: 'me', component: MessangerPage}
+  { path: '', component: GreetPage },
+  { path: 'me', component: MessangerPage },
 ]
---
+++ src/app/features/messanger-page/components/menu-component/menu-component.ts
@@ -0,0 +1,43 @@
+import { Component, ElementRef, ViewChild, OnInit, OnDestroy } from '@angular/core'
+import { TranslateModule } from '@ngx-translate/core'
+import { animate } from 'motion'
+import { fromEvent, Subject, takeUntil } from 'rxjs'
+@Component({
+  selector: 'app-menu-component',
+  imports: [TranslateModule],
+  templateUrl: './menu-component.html',
+  styleUrl: './menu-component.scss',
+})
+export class MenuComponent {
+  @ViewChild('menu') menu!: ElementRef
+  @ViewChild('overlay') overlay!: ElementRef
+  openMenu() {
+    const menu = this.menu.nativeElement
+    const close$ = new Subject<void>()
+    menu.style.transform = 'translateX(0%)'
+    animate(
+      menu,
+      { opacity: ['0%', '100%'] },
+      {
+        duration: 0.5,
+      },
+    )
+    fromEvent<PointerEvent>(document, 'pointerdown') //подписка на закрытие меню
+      .pipe(takeUntil(close$))
+      .subscribe((e) => {
+        if (!this.overlay.nativeElement.contains(e.target)) {
+          animate(
+            menu,
+            { opacity: ['100%', '0%'] },
+            {
+              duration: 0.5,
+            },
+          )
+          setTimeout(() => {
+            menu.style.transform = 'translateX(100%)'
+            close$.next()
+          }, 500)
+        }
+      })
+  }
+}
